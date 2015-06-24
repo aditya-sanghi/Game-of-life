fcfcb7ebd4b9e78a05a5645555d08095e7d59e5d
Aditya Sanghi <adityasanghi@precious-9.local>
2015-06-24 21:02:33 +0530

    converted tabs to whitespaces

--- lib/game_of_life/cell.rb
+++ lib/game_of_life/cell.rb
@@ -1,24 +1,24 @@
 
 module GameOfLife
-	class Cell
-		attr_reader :x, :y
+  class Cell
+    attr_reader :x, :y
 
-		def initialize (x, y)
-			@x = x
-			@y = y
-			@alive = false
-		end
+    def initialize (x, y)
+      @x = x
+      @y = y
+      @alive = false
+    end
 
-		def alive?
-			@alive
-		end
+    def alive?
+      @alive
+    end
 
-		def dead?
-			!@alive
-		end
+    def dead?
+      !@alive
+    end
 
-		def revive!
-			@alive = true
-		end
-	end
+    def revive!
+      @alive = true
+    end
+  end
 end
--- spec/game_of_life/cell_spec.rb
+++ spec/game_of_life/cell_spec.rb
@@ -1,37 +1,37 @@
 require_relative '../spec_helper'
 
 module GameOfLife
-	describe Cell do
-		it '#initialization' do
-			cell = Cell.new(1, 1)
-			expect(cell.x).to eq(1)
-			expect(cell.y).to eq(1)
-		end
+  describe Cell do
+    it '#initialization' do
+      cell = Cell.new(1, 1)
+      expect(cell.x).to eq(1)
+      expect(cell.y).to eq(1)
+    end
 
-		describe '#dead?' do
-			it 'should be dead by default' do
-				cell = Cell.new(1, 1)
-				expect(cell).to be_dead
-			end
+    describe '#dead?' do
+      it 'should be dead by default' do
+        cell = Cell.new(1, 1)
+        expect(cell).to be_dead
+      end
 
-			it 'should not be dead after revival' do
-				cell = Cell.new(1, 1)
-				cell.revive!
-				expect(cell).to_not be_dead
-			end
-		end
+      it 'should not be dead after revival' do
+        cell = Cell.new(1, 1)
+        cell.revive!
+        expect(cell).to_not be_dead
+      end
+    end
 
-		describe '#alive?' do
-			it 'should not be alive by default' do
-				cell = Cell.new(1, 1)
-				expect(cell).not_to be_alive
-			end
+    describe '#alive?' do
+      it 'should not be alive by default' do
+        cell = Cell.new(1, 1)
+        expect(cell).not_to be_alive
+      end
 
-			it 'should be alive after revival' do
-				cell = Cell.new(1, 1)
-				cell.revive!
-				expect(cell).to be_alive
-			end
-		end
-	end
+      it 'should be alive after revival' do
+        cell = Cell.new(1, 1)
+        cell.revive!
+        expect(cell).to be_alive
+      end
+    end
+  end
 end
