9218c1022c14f333a9a89a0edaf8d40822f2f247
Aditya Sanghi <adityasanghi@precious-9.local>
2015-07-10 15:11:59 +0530

    added dead and alive attributes to cell

--- spec/game_of_life/cell_spec.rb
+++ spec/game_of_life/cell_spec.rb
@@ -1,45 +1,41 @@
-require 'spec_helper'
+#require 'spec_helper'
+require_relative '../../lib/game_of_life/cell'
 
 module GameOfLife
   describe Cell do
 
-    describe '#initialization' do
-      it 'should be dead by default' do
-        cell = Cell.new
-        expect(cell).to be_dead
-      end
-    end
-
     describe '#a cell that is dead' do
+
       it 'should not be dead after revival' do
-          cell = Cell.new
+          cell = Cell.new(Cell:: DEAD)
           cell.revive!
           expect(cell).to_not be_dead
       end
 
       it 'should be alive after revival' do
-        cell = Cell.new
+        cell = Cell.new(Cell:: DEAD)
         cell.revive!
         expect(cell).to be_alive
       end
 
       it 'should not be alive if no operation performed on it' do
-        cell = Cell.new
+        cell = Cell.new(Cell:: DEAD)
         expect(cell).not_to be_alive
+      end
+
+        it 'should be dead if no operation performed on it' do
+          cell = Cell.new(Cell:: DEAD)
+          expect(cell).to be_dead
       end
     end
 
     describe 'a cell that is alive' do
-          end
-
-    describe '#kill!' do
-      it 'should make the cell dead' do
-        cell = Cell.new
+      it 'should be alive if no operation performed on it' do
+        cell = Cell.new(Cell:: ALIVE)
         cell.revive!
         cell.kill!
-        expect(cell). to be_dead
+        expect(cell). to be_alive
       end
     end
-
   end
 end
